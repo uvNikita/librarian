6a9e19ce84c5527d08401f2581b771a8d1274b6e
Nikita Uvarov <uv.nikita@gmail.com>
2012-05-12 18:27:25 +0300

    Syntax error fixed

--- db/models.py
+++ db/models.py
@@ -2,4 +2,4 @@ from collections import namedtuple
 
 Book = namedtuple('Book', ['book_id', 'title', 'authors', 'genre', 'sequence', 'annotation'])
     
-Author = namedtuple('Author', ['author_id', 'first_name', 'last_name']
+Author = namedtuple('Author', ['author_id', 'first_name', 'last_name'])
