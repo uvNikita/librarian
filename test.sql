2ac4ab1c3047f68769400c4e497951620e1f67aa
Nikita Uvarov <uv.nikita@gmail.com>
2012-05-19 12:23:16 +0300

    Added test.sql for internal tests

---
+++ test.sql
@@ -0,0 +1,3 @@
+insert into book values (1,"Mort", "Annotation. Must be a Huge annotation here.", "Mort seq", 1);
+insert into author(first_name, last_name) values ("John", "Doe");
+insert into author_book values (1,1);
