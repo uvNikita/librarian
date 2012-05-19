baa33e8865071f15ee95b71c6f788f7c82611245
Nikita Uvarov <uv.nikita@gmail.com>
2012-05-19 13:23:56 +0300

    Expanded test.sql

--- test.sql
+++ test.sql
@@ -1,3 +1,8 @@
 insert into book values (1,"Mort", "Annotation. Must be a Huge annotation here.", "Mort seq", 1);
+insert into book values (2,"Mortana", NULL, "Mort seq", 2);
+insert into book values (3,"Lird", "Realy big, big, huge annotation must be here. Hope it'sbig enough..", NULL, NULL);
 insert into author(first_name, last_name) values ("John", "Doe");
+insert into author(first_name, last_name) values ("Jane", "Doe");
 insert into author_book values (1,1);
+insert into author_book values (1,2);
+insert into author_book values (2,3);
