c0177e79e3fe0324502daef8bb5282518321c9bf
Nikita Uvarov <uv.nikita@gmail.com>
2012-07-28 14:20:39 +0300

    Added init_db command in manage.py (no testing)

--- manage.py
+++ manage.py
@@ -3,8 +3,14 @@
 from flaskext.script import Manager
 
 from librarian import app
+from librarian.models import db
 
 manager = Manager(app)
+
+@manager.command
+def init_db():
+    db.drop_all()
+    db.create_all()
 
 if __name__ == "__main__":
     manager.run()
