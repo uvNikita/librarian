7c1c1d10b5f61e747c9b9681e47f6e804d15acef
Nikita Uvarov <uv.nikita@gmail.com>
2012-08-25 21:44:27 +0300

    Add redirect from main_page to authors_chooser. Move books_sorted and authors_sorted to utils

--- librarian/main/main.py
+++ librarian/main/main.py
@@ -4,8 +4,10 @@ from os import listdir, path, rename
 from zipfile import ZipFile
 
 from flask import Blueprint, current_app, request, render_template, abort
+from flask import redirect, url_for
 from flask import send_from_directory
 
+from librarian.util import books_sorted, authors_sorted
 from librarian.models import Book, Author
 
 
@@ -16,24 +18,9 @@ main = Blueprint('main', __name__, template_folder='templates',
                  static_folder='static')
 
 
-def books_sorted(query):
-    return (
-        query
-        .order_by(Book.sequence_id)
-        .order_by(Book.sequence_number)
-    )
-
-
-def authors_sorted(query):
-    return (
-        query
-        .order_by(Author.last_name)
-    )
-
-
 @main.route("/")
 def main_page():
-    return render_template("main_page.html")
+    return redirect(url_for('.authors_chooser'))
 
 
 @main.route("/b/<int:book_id>")
@@ -61,7 +48,8 @@ def author_books(author_id, page):
     if not author:
         abort(404)
     books_pager = books_sorted(author.books).paginate(page, ITEMS_PER_PAGE)
-    return render_template("books_list.html", author=author, books_pager=books_pager)
+    return render_template("books_list.html", author=author,
+                           books_pager=books_pager)
 
 
 @main.route("/search", defaults={'page': 1})
--- librarian/util.py
+++ librarian/util.py
@@ -2,9 +2,27 @@
 
 from flask import request, url_for
 
+from librarian.models import Book, Author
+
+
 def current_url(save_get_params=False, **updates):
      args = request.view_args.copy()
      args.update(updates)
      if save_get_params:
          args.update(request.args)
      return url_for(request.endpoint, **args)
+
+
+def books_sorted(query):
+    return (
+        query
+        .order_by(Book.sequence_id)
+        .order_by(Book.sequence_number)
+    )
+
+
+def authors_sorted(query):
+    return (
+        query
+        .order_by(Author.last_name)
+    )
