d75fe09566b783a746983c64cfb35b33caa61721
Nikita Uvarov <uv.nikita@gmail.com>
2012-09-02 21:43:15 +0300

    Remove DEBUG=True option from config

--- config.py
+++ config.py
@@ -4,4 +4,3 @@ DATABASE = "dbname=library user=librarian password=librarian_pass host=192.168.1
 SQLALCHEMY_DATABASE_URI = 'postgresql://librarian:librarian_pass@192.168.138.100/library'
 PATH_TO_LIBRARY = '/media/Y/Библиотека/'
 TEMPORARY_FOLDER = '/tmp/'
-DEBUG = True
