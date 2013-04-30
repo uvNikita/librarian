5696b923ad1f400327372a88515c968eed4f1c7e
Nikita Uvarov <uv.nikita@gmail.com>
2013-04-30 18:05:16 +0300

    Ranamed .jinja to .html and .xml back

--- librarian/opds/opds.py
+++ librarian/opds/opds.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, render_template, request, abort
+from flask import Blueprint, Response, render_template, request, abort
 
 from librarian.util import books_sorted, authors_sorted
 from librarian.models import Author
@@ -31,20 +31,23 @@ def authors_chooser():
 
     if show_authors:
         authors = Author.query.filter(Author.last_name.ilike(curr_prefix))
-        return render_template('opds_authors.jinja', authors=authors)
+        return render_template('opds_authors.xml', authors=authors)
 
     authors = Author.search_starting_from(curr_prefix)
     if authors.count() < AUTHORS_PER_PAGE:
         authors = authors_sorted(authors)
-        return render_template('opds_authors.jinja', authors=authors)
+        return render_template('opds_authors.xml', authors=authors)
     prefixes = []
     for char in chars:
         new_prefix = curr_prefix + char
         count = Author.search_starting_from(new_prefix).count()
         if count:
             prefixes.append((new_prefix, count))
-    return render_template('opds_authors_chooser.jinja', curr_prefix=curr_prefix,
-                           prefixes=prefixes, lang=lang)
+    return Response(
+        render_template(
+            'opds_authors_chooser.xml', curr_prefix=curr_prefix,
+            prefixes=prefixes, lang=lang),
+        mimetype='text/xml')
 
 
 @opds.route('/a-<int:author_id>')
@@ -53,4 +56,6 @@ def author_books(author_id):
     if not author:
         abort(404)
     books = books_sorted(author.books)
-    return render_template('opds_books_list.jinja', author=author, books=books)
+    return Response(
+        render_template('opds_books_list.xml', author=author, books=books),
+        mimetype='text/xml')
