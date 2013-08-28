9487aabdf99133d2d328413aee9d55913cec3d15
Nikita Uvarov <uv.nikita@gmail.com>
2013-08-29 00:36:06 +0300

    Moved .inp file parsing to celery. Implemented add_books_from_inpx celery task

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-DATABASE = "dbname=library user=librarian password=librarian_pass host=192.168.138.100"
 SQLALCHEMY_DATABASE_URI = 'postgresql://librarian:librarian_pass@192.168.138.100/library'
 PATH_TO_LIBRARY = '/media/Y/Библиотека/'
 TEMPORARY_FOLDER = '/tmp/'
--- librarian/tasks.py
+++ librarian/tasks.py
@@ -1,5 +1,8 @@
 #  coding: utf-8
-
+import logging
+import tempfile
+import os
+import shutil
 from functools import wraps
 from zipfile import ZipFile
 
@@ -7,8 +10,10 @@ from lxml import etree
 from celery import Celery
 
 from librarian import app
-from librarian.models import db, Book
+from librarian.models import db, Book, Author, Genre, Sequence
 
+
+log = logging.getLogger(__name__)
 
 celery = Celery(__name__)
 celery.conf.update(app.config)
@@ -19,19 +24,92 @@ def with_context(task):
     def wrapper(*args, **kwargs):
         with app.test_request_context():
             return task(*args, **kwargs)
+
     return wrapper
 
 
 @celery.task
 @with_context
 def add_books_from_inp(path):
-    print "Hello from add_books_from_inp"
+    """
+    Add books and create all need entities from parsed .inp file
+    :type path: str
+    :param path: path to .inp file
+    """
+    log.info("Start to process {file}".format(file=path))
+    with open(path) as inp_file:
+        for line in inp_file:
+            (unparsed_authors, gen_titles, title,
+             seq_title, sequence_number, id_) = line.split('\x04')[:6]
+            unparsed_authors = unparsed_authors.split(':')[:-1]
+            authors = []
+            for unparsed_author in unparsed_authors:
+                unparsed_author = unparsed_author.split(',')
+                last_name = unparsed_author[0]
+                first_name = unparsed_author[1] or None
+                db_author = Author.query.filter_by(
+                    first_name=first_name,
+                    last_name=last_name
+                ).first()
+                if db_author:
+                    authors.append(db_author)
+                else:
+                    authors.append(Author(
+                        first_name=first_name,
+                        last_name=last_name
+                    ))
+
+            gen_titles = gen_titles.split(':')[:-1]
+            genres = []
+            for gen_title in gen_titles:
+                db_genre = Genre.query.filter_by(title=gen_title).first()
+                if db_genre:
+                    genres.append(db_genre)
+                else:
+                    genres.append(Genre(title=gen_title))
+
+            if seq_title:
+                db_sequence = Sequence.query.filter_by(title=seq_title).first()
+                sequence = db_sequence or Sequence(title=seq_title)
+            else:
+                sequence = None
+            sequence_number = int(sequence_number) if sequence_number else None
+
+            id_ = int(id_)
+            book = Book(
+                id=id_,
+                title=title,
+                authors=authors,
+                genres=genres,
+                sequence=sequence,
+                sequence_number=sequence_number,
+                annotation=None
+            )
+
+            db.session.add(book)
+            db.session.flush()
+        db.session.commit()
+    log.info("Finish to process {file}".format(file=path))
 
 
 @celery.task
 @with_context
 def add_books_from_inpx(path):
-    pass
+    tmp_path = tempfile.mkdtemp(prefix='librarian.', suffix='-inpx')
+    log.info("Extracting archive {a} to {d}".format(a=path, d=tmp_path))
+    with ZipFile(path) as inpx_file:
+        names = inpx_file.namelist()
+        names = filter(lambda name: name.endswith('.inp'), names)
+        for name in names:
+            inpx_file.extract(name, tmp_path)
+    log.info("Done")
+    log.info("Going to process files: {files}".format(files=names))
+    try:
+        for name in names:
+            inp_file_path = os.path.join(tmp_path, name)
+            add_books_from_inp(inp_file_path)
+    finally:
+        shutil.rmtree(tmp_path)
 
 
 FB2_NS = 'http://www.gribuser.ru/xml/fictionbook/2.0'
